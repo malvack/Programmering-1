216d34c6c2179becf201df0c7c3f3d92a4da1bd8
Martin Olsson <malvack@gmail.com>
2019-02-21 22:19:35 +0100

    Uppgift 0.2. Extra uppgifter

--- Uppgift1/Program.cs
+++ Uppgift1/Program.cs
@@ -6,17 +6,18 @@ namespace Uppgift1
     {
         static void Main(string[] args)
         {
+            /*
             /*  
                 Deklarera variabler.
-                Valde string för fNamn, eNamn och alder då jag läser in värden i med ReadLine() funktionen.
-                ReadLine() läser enbart in string värden.
-                Valde int för antalDagar då jag använder denna i matematiska beräkningar.
+                Valde string för fNamn, eNamn och då jag läser in värden i med ReadLine() funktionen.
+                ReadLine() läser enbart in string värden om inget annat anges.
+                Valde int för alder, antalDagar då jag använder matematiska beräkningar för dessa.
             */
-            string fNamn, eNamn, alder;
-            int antalDagar;
+            string fNamn, eNamn;
+            int alder, antalDagar;
 
             // Läs in för- och efternamn i variablarna fNamn, eNamn och hälsa välkommen.
-            Console.WriteLine("----------- Uppgift 1 -----------");
+            Console.WriteLine("------------ Uppgift 1 ------------");
             Console.WriteLine("Ange ditt förnamn:");
             fNamn = Console.ReadLine();
             Console.WriteLine("Ange ditt efternamn:");
@@ -25,13 +26,42 @@ namespace Uppgift1
 
             // Läs in ålder och spara i string variablen alder.
             Console.WriteLine("Hur gammal är du {0}?", fNamn);
-            alder = Console.ReadLine();
 
-            // Konventera om alder str till int32 och mulitiplicera med 365 dagar. Laga resultatet i variabeln dagar. //
-            antalDagar = Convert.ToInt32(alder) * 365;
+            // Acceptera enbart numeriska värden i variablen alder
+            while (!int.TryParse(Console.ReadLine(), out alder))
+            {
+                Console.WriteLine("Du måste ange ett numeriskt värde!");
+            }
+
+            // Mulitiplicera alder med 365 dagar. Laga resultatet i variabeln antalDagar. //
+            antalDagar = alder * 365;
 
             // Skriv ut värdet i int variablen dagar.
-            Console.WriteLine("{0}, du är {1} dagar gammal!", fNamn, antalDagar);
+            Console.WriteLine("Visste du {0}, att du är {1} dagar gammal!", fNamn, antalDagar);
+
+            // Extra uppgifter //
+            Console.WriteLine("---------- Extra Uppgift ----------");
+
+            // Spara dagens datum i idag variablen.
+            DateTime idag = DateTime.Now;
+            DateTime birthday;
+
+            // Fråga efter användarens födelsedag och lagra värdet i birthday variabeln.
+            Console.WriteLine("Ange din födelsedag i formatet åååå-mm-dd:");
+            while (!DateTime.TryParse(Console.ReadLine(), out birthday))
+            {
+                Console.WriteLine("Du måste ange din födelsedag i formatet åååå-mm-dd!");
+            }
+
+            // Räkna ut antal timmar, minuter och sekunder användaren har levt.
+            TimeSpan tid = idag - birthday;
+            int totalTimmar = Convert.ToInt32(tid.TotalHours);
+            int totalMinuter = Convert.ToInt32(tid.TotalMinutes);
+            int totalSekunder = Convert.ToInt32(tid.TotalSeconds);
+
+            // Presentera resultatet för användaren
+            Console.WriteLine("Du har levt i {0} timmar eller {1} minuter eller {2} sekunder.", totalTimmar, totalMinuter, totalSekunder);
+
         }
     }
 }
